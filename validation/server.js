b8dcb4c85b69982a00823d45a1b9f765203e522e
dannylopez2284 <92945169+dannylopez2284@users.noreply.github.com>
2021-10-22 07:46:52 +0800

    Update server.js

--- validation/server.js
+++ validation/server.js
@@ -3,6 +3,6 @@ const hostname = "192.168.0.101";
 const port = process.env.port || 5000;
 const app = require("./app");
 
-app.listen(port, hostname, () => {
+app.listen(port,  () => {
   console.log(" Server running at  " + port);
 });
